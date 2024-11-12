9d9d64e5bffd30bbb7b00c51c41b8fdbd2189e3b
нет нет <tkak4268@gmail.com>
2024-11-12 09:34:33 +0700

    CheckLinks() переделать метод для проверки связей

--- ConsoleApp2/BuildingManager.cs
+++ ConsoleApp2/BuildingManager.cs
@@ -34,7 +34,7 @@ namespace ConsoleApp2
         public (int, int, int) Maximum { get => _maximum; set => _maximum = value; }
 
         #endregion
-        public void CreateRooms() 
+        public void CreateRooms()
         {
 
             Room room;
@@ -57,32 +57,36 @@ namespace ConsoleApp2
              2 = в право
              -2 = в лево
              0 = вверх по этажу
+             3 = вниз по этажу
         */
         private void Method() // назвать по другому
         {
 
-            while (LinksRoom.Count != 10)
+            while (LinksRoom.Count != 5)
             {
-                int move = rnd.Next(-2, 2);
+                int move = rnd.Next(-2, 3);
                 switch (move)
                 {
-                    case 1:
+                    case 1: //вверх
                         SetLinks(1);
                         break;
-                    case -1:
+                    case -1://вниз
                         SetLinks(-1);
 
                         break;
-                    case 2:
+                    case 2://в право
                         SetLinks(2);
 
                         break;
-                    case -2:
+                    case -2://в лево
                         SetLinks(-2);
 
                         break;
-                    case 0:
+                    case 0://вверх по этажу
                         SetLinks(0);
+                        break;
+                    case 3://вниз по этажу
+                        SetLinks(3);
                         break;
                 }
 
@@ -97,39 +101,38 @@ namespace ConsoleApp2
         }
         private void SetLinks(int move)
         {
-            move = 1;
             if (CurrentRoom.Coord.Item2 < Maximum.Item2 && CurrentRoom.Coord.Item2 >= 0)   // вверх и вниз
             {
                 if (move == 1)
                 {
-                    //if (SecondsRooms.Contains(new Room(CurrentRoom.Coord.Item1, CurrentRoom.Coord.Item2 + 1)))
-                    //{
+                    if (CheckContainceByCoords(SecondsRooms, CurrentRoom, move))
+                    {
                         foreach (var item in SecondsRooms)
                         {
-                            if (item.Coord.Item2 == CurrentRoom.Coord.Item2 + 1 && CheckLinks(CurrentRoom, item) == false)
+                            if (item.Coord.Item2 == CurrentRoom.Coord.Item2 + 1 && CheckLinks(CurrentRoom, item))
                             {
                                 LinksRoom.Add(CurrentRoom, new List<Room>() { item });
                                 CurrentRoom = item;
                                 return;
                             }
                         }
-                    //}
+                    }
                 }
                 if (move == -1)
                 {
-                    //if (SecondsRooms.Contains(new Room(CurrentRoom.Coord.Item1, CurrentRoom.Coord.Item2 - 1)))
-                    //{
+                    if (CheckContainceByCoords(SecondsRooms, CurrentRoom, move))
+                    {
                         foreach (var item in SecondsRooms)
                         {
 
-                            if (item.Coord.Item2 == CurrentRoom.Coord.Item2 - 1 && CheckLinks(CurrentRoom, item) == false)
+                            if (item.Coord.Item2 == CurrentRoom.Coord.Item2 - 1 && CheckLinks(CurrentRoom, item))
                             {
                                 LinksRoom.Add(CurrentRoom, new List<Room>() { item });
                                 CurrentRoom = item;
                                 return;
                             }
                         }
-                    //}
+                    }
                 }
             }
 
@@ -137,33 +140,33 @@ namespace ConsoleApp2
             {
                 if (move == 2)
                 {
-                    //if (SecondsRooms.Contains(new Room(CurrentRoom.Coord.Item1 +1, CurrentRoom.Coord.Item2)))
-                    //{
+                    if (CheckContainceByCoords(SecondsRooms, CurrentRoom, move))  
+                    {
                         foreach (var item in SecondsRooms)
                         {
-                            if (item.Coord.Item1 == CurrentRoom.Coord.Item1 + 1 && CheckLinks(CurrentRoom, item) == false)
+                            if (item.Coord.Item1 == CurrentRoom.Coord.Item1 + 1 && CheckLinks(CurrentRoom, item))
                             {
                                 LinksRoom.Add(CurrentRoom, new List<Room>() { item });
                                 CurrentRoom = item;
                                 return;
                             }
                         }
-                    //}
+                    }
                 }
                 if (move == -2)
                 {
-                    //if (SecondsRooms.Contains(new Room(CurrentRoom.Coord.Item1 - 1, CurrentRoom.Coord.Item2)))
-                    //{
+                    if (CheckContainceByCoords(SecondsRooms, CurrentRoom, move))
+                    {
                         foreach (var item in SecondsRooms)
                         {
-                            if (item.Coord.Item1 == CurrentRoom.Coord.Item1 - 1 && CheckLinks(CurrentRoom, item) == false)
+                            if (item.Coord.Item1 == CurrentRoom.Coord.Item1 - 1 && CheckLinks(CurrentRoom, item))
                             {
                                 LinksRoom.Add(CurrentRoom, new List<Room>() { item });
                                 CurrentRoom = item;
                                 return;
                             }
                         }
-                    //}
+                    }
                 }
             }
             /* if (move == 0)
@@ -188,25 +191,79 @@ namespace ConsoleApp2
                      break;
                  }
              }*/
-        }
-        private bool CheckLinks(Room Key, Room Value) // посмотреть может проблема в корявой выдачи связей связан с этом методом
-        {
-            foreach (var item in LinksRoom.Keys)
+            /*if (move == 3)
             {
-                foreach (var item2 in LinksRoom.Values)
+                foreach (var item in _floors)
                 {
-                    foreach (var item3 in item2)
+                    foreach (var item1 in item.Rooms)
                     {
-                        if (item == Key && item3 == Value || item == Value && item3 == Key)
+
+                    }
+                }
+
+
+                foreach (var item in SecondsRooms)
+                {
+                    if (CheckLinks(CurrentRoom, item) == false)
+                    {
+                        LinksRoom.Add(CurrentRoom, new List<Room>() { item });
+                        CurrentRoom = item;
+                        return;
+                    }
+                    break;
+                }
+            }*/
+        }
+
+        private bool CheckContainceByCoords(List<Room> secondsRooms, Room currentRoom, int move)
+        {
+            foreach (var item in secondsRooms)
+            {
+                switch (move)
+                {
+                    case 1:
+                        if (item.Coord.Item1 == currentRoom.Coord.Item1 && item.Coord.Item2 == currentRoom.Coord.Item2 + 1) return true;
+                        break;
+                    case -1:
+                        if (item.Coord.Item1 == currentRoom.Coord.Item1 && item.Coord.Item2 == currentRoom.Coord.Item2 - 1) return true;
+                        break;
+                    case 2:
+                        if (item.Coord.Item1 == currentRoom.Coord.Item1 + 1 && item.Coord.Item2 == currentRoom.Coord.Item2) return true;
+                        break;
+                    case -2:
+                        if (item.Coord.Item1 == currentRoom.Coord.Item1 - 1 && item.Coord.Item2 == currentRoom.Coord.Item2) return true;
+                        break;
+                    case 0:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckLinks(Room CheckedKey, Room CheckedValue) // сделать тоже самое только по кординатам потому что создает новый объект класса и занимает другую память
+        {
+            foreach (var key in LinksRoom.Keys)
+            {
+                foreach (var values in LinksRoom.Values)
+                {
+                    foreach (var value in values)
+                    {//     0,0     1,0           1,0       0,0           0,0          0,0
+                        Console.WriteLine(key);
+                        Console.WriteLine(CheckedValue);
+                        if (key == CheckedKey && value == CheckedValue || key == CheckedValue && value == CheckedKey)
                         {
                             Console.WriteLine("такая связь уже есть");
-                            return true;
+                            return false;
                         }
                     }
                 }
             }
-            return false;
+            return true;
         }
+
         #region InfoMethods
         public void ShowInfo()
         {
